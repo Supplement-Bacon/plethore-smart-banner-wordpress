3443975a721d018397d8cd4fc40d84ab6c583110
Samuel HASSID <samuel@supplement-bacon.com>
2025-03-27 01:16:30 +0100

    Add uninstall.php

---
+++ uninstall.php
@@ -0,0 +1,11 @@
+<?php
+
+// if uninstall.php is not called by WordPress, die
+if (! defined('WP_UNINSTALL_PLUGIN')) {
+    die;
+}
+
+unregister_setting(
+    'plethore-smart-banner-setup',
+    Plethore_Smart_Banner::SETTING_ACCOUNT_SLUG
+);
